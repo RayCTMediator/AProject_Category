ec3d92ba7536ba5259577f4a353aa0cf503441aa
吴孔亮 <ray_ios@163.com>
2019-09-29 16:53:17 +0800

    init

---
+++ AProject_Category/AProject_Category/CTMediator+A.h
@@ -0,0 +1,21 @@
+//
+//  CTMediator+A.h
+//  AProject_Category
+//
+//  Created by WKL on 2019/9/29.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+
+@interface CTMediator (A)
+
+-(UIViewController*)A_aViewController;
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+
+@end
+
+
---
+++ AProject_Category/AProject_Category/CTMediator+A.m
@@ -0,0 +1,32 @@
+//
+//  CTMediator+A.m
+//  AProject_Category
+//
+//  Created by WKL on 2019/9/29.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+#import "CTMediator+A.h"
+
+@implementation CTMediator (A)
+
+- (UIViewController *)A_aViewController
+{
+    /*
+     AViewController *viewController = [[AViewController alloc] init];
+     */
+    return [self performTarget:@"A" action:@"viewController" params:nil shouldCacheTarget:NO];
+}
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText
+{
+    /*
+     BViewController *viewController = [[BViewController alloc] initWithContentText:@"hello, world!"];
+     */
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"contentText"] = contentText;
+    return [self performTarget:@"BProject" action:@"viewController" params:params shouldCacheTarget:NO];
+    
+ }
+
+@end
